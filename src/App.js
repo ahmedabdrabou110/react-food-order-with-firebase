5bfbece5cf9da14c0275f4fd0f3252f65e48b61e
Ahmed Abdrabou <a.abdrabou110@icloud.com>
2022-11-28 18:21:13 +0200

    sending data with notification with success , pending and error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
+import { uiActions } from "./components/store/ui-slice";
+import Notifications from "./components/UI/Notifications";
+
+let isInitial = true;
+
 function App() {
-  const ui = useSelector((state) => state.ui.cartIsVisible);
+  const dispatch = useDispatch();
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
   useEffect(() => {
-    fetch(
-      "https://react-food-order-1098c-default-rtdb.firebaseio.com/cart.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data still pending!",
+        })
+      );
+      const response = await fetch(
+        "https://react-food-order-1098c-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
       }
-    );
-  }, [cart]);
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
+
   return (
-    <Layout>
-      {ui && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notifications
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
--- src/components/UI/Notifications.js
+++ src/components/UI/Notifications.js
@@ -1,18 +1,24 @@
-import React from "react";
 import classes from "./Notifications.module.css";
+
 const Notifications = (props) => {
   let specialClasses = "";
-  const cssClasses = `${classes.notification} ${specialClasses}`;
+
   if (props.status === "error") {
     specialClasses = classes.error;
   }
-
   if (props.status === "success") {
     specialClasses = classes.success;
   }
+
+  if (props.status === "pending") {
+    specialClasses = classes.pending;
+  }
+
+  const cssClasses = `${classes.notification} ${specialClasses}`;
+
   return (
     <section className={cssClasses}>
-      <h1>{props.title}</h1>
+      <h2>{props.title}</h2>
       <p>{props.message}</p>
     </section>
   );
